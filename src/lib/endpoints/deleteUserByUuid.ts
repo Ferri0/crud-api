0c966dcf732188d121268b9f65b777447b696037
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2023-01-16 23:12:43 +0100

    feat: implement deleteUserByUuid endpoint

---
+++ src/lib/endpoints/deleteUserByUuid.ts
@@ -0,0 +1,30 @@
+import { validate } from 'uuid';
+import { IncomingMessage, ServerResponse } from 'node:http';
+
+/**
+ * Endpoint based on '/api/users/${uuid}' route
+ * Accepts DELETE requests
+ */
+export const deleteUserByUuid = (req: IncomingMessage, res: ServerResponse, userId: string) => {
+    try {
+        if (validate(userId)) {
+            const userData = DataStorageInstance.getUserById(userId);
+
+            if (userData) {
+                DataStorageInstance.deleteUserById(userId);
+            } else {
+                res.statusCode = 404;
+                throw new Error('User not found');
+            }
+        } else {
+            res.statusCode = 400;
+            throw new Error('User ID is not valid');
+        }
+    } catch (error: any) {
+        if (res.statusCode !== 400 && res.statusCode !== 404) {
+            res.statusCode = 500;
+        }
+
+        res.end(error.message);
+    }
+};
