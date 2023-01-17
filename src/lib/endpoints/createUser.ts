295d0aa0ce3f8f82a6243f414001e8ab3c0de4ec
Yaroslav Abrasimov <y.abrasimov@atwix.com>
2023-01-17 12:59:05 +0100

    refactor: createUser endpoint

--- src/lib/endpoints/createUser.ts
+++ src/lib/endpoints/createUser.ts
@@ -1,36 +1,28 @@
 import { IncomingMessage, ServerResponse } from 'node:http';
 
 import { validateUserData } from '../../services/validateUserData';
+import { getRequestData } from '../../services/getRequestData';
 
 /**
  * Endpoint based on '/api/users' route
  * Accepts POST requests with user data
  */
-export const createUser = (req: IncomingMessage, res: ServerResponse) => {
-    const chunks: Uint8Array[] = [];
+export const createUser = async (req: IncomingMessage, res: ServerResponse) => {
+    try {
+        const userData = await getRequestData(req);
+        const parsedUserData = JSON.parse(userData);
 
-    req.on('data', (chunk) => {
-        chunks.push(chunk);
-    });
+        if (validateUserData(parsedUserData, 'POST')) {
+            const generatedRecord = DataStorageInstance.createUser(parsedUserData);
 
-    req.on('end', () => {
-        try {
-            const dataBuffer = Buffer.concat(chunks);
-            const dataString = dataBuffer.toString();
-            const reqBody = JSON.parse(dataString);
-
-            if (validateUserData(reqBody, 'POST')) {
-                const generatedRecord = DataStorageInstance.createUser(reqBody);
-
-                res.statusCode = 201;
-                res.end(generatedRecord);
-            } else {
-                res.statusCode = 400;
-                res.end('Invalid user data provided');
-            }
-        } catch (error: any) {
-            res.statusCode = 500;
-            res.end(error.message);
+            res.statusCode = 201;
+            res.end(generatedRecord);
+        } else {
+            res.statusCode = 400;
+            res.end('Invalid user data provided');
         }
-    });
+    } catch (error: any) {
+        res.statusCode = 500;
+        res.end(error.message);
+    }
 };
